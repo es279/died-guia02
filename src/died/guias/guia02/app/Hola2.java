c8de6e799985824e288dadea9d076fb9ee823268
es279 <emirsonzogni279@gmail.com>
2018-04-08 02:20:47 -0300

    version inicial nicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author usuario
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("el tiempo actual en milis es "+
+				System.currentTimeMillis());
+
+	}
+
+}
